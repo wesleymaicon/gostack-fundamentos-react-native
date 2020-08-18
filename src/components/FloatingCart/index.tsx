2dbc9ac928f6a2bf8a708ab3576ee4f55a18d453
wesleymaicon <wesleymaicon002@gmail.com>
2020-08-18 14:52:37 -0300

    list, add and remove products from cart

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -15,24 +15,27 @@ import formatValue from '../../utils/formatValue';
 
 import { useCart } from '../../hooks/cart';
 
-// Calculo do total
-// Navegação no clique do TouchableHighlight
-
 const FloatingCart: React.FC = () => {
   const { products } = useCart();
 
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productsTotal = product.price * product.quantity;
 
-    return formatValue(0);
+      return accumulator + productsTotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      return accumulator + product.quantity;
+    }, 0);
 
-    return 0;
+    return total;
   }, [products]);
 
   return (
